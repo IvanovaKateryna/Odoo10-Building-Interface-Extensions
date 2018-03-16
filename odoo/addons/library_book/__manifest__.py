7bf87618771c2fc94fedf1be7155fa7f1f933abb
IvanovaKateryna <kis.iv333@gmail.com>
2018-03-16 15:12:55 +0200

    [ADD] Create library.book module

---
+++ odoo/addons/library_book/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library book",
+
+    'summary': """Library book""",
+
+    'description': """Library book""",
+
+    'author': "Ivanova Kateryna",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': [
+        'base',
+        'website',
+    ],
+
+    'data': [
+        'views/snippets.xml',
+    ],
+
+}
